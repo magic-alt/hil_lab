ff2943212c4e348932ac63ad8ae48d650d9842a1
kaermaxc <59996146+magic-alt@users.noreply.github.com>
2026-09-18 22:04:34 +0800

    fix: remove legacy custom INTC resource from B0 table

--- boards/beaglebone_black/firmware/pru0_b0/resource_table_0.c
+++ boards/beaglebone_black/firmware/pru0_b0/resource_table_0.c
@@ -1,22 +1,11 @@
 #include "resource_table_0.h"
 
-/*
- * RPMsg system events for PRU0.
- *   16: PRU0 -> ARM, route through channel 2 -> host 2
- *   17: ARM -> PRU0, route through channel 0 -> host 0 (R31 bit 30)
- */
-struct ch_map pru_intc_map[] = {
-    { 16u, 2u },
-    { 17u, 0u },
-};
-
 #pragma DATA_SECTION(resourceTable, ".resource_table")
 #pragma RETAIN(resourceTable)
 struct hil_b0_resource_table resourceTable = {
-    { 1u, 2u, 0u, 0u },
+    { 1u, 1u, { 0u, 0u } },
     {
-        offsetof(struct hil_b0_resource_table, rpmsg_vdev),
-        offsetof(struct hil_b0_resource_table, pru_ints)
+        offsetof(struct hil_b0_resource_table, rpmsg_vdev)
     },
     {
         (uint32_t)TYPE_VDEV,
@@ -42,19 +31,5 @@ struct hil_b0_resource_table resourceTable = {
         PRU_RPMSG_VQ1_SIZE,
         0u,
         0u
-    },
-    {
-        TYPE_POSTLOAD_VENDOR,
-        (PRU_INTS_VER0 | TYPE_PRU_INTS),
-        sizeof(struct fw_rsc_custom_ints),
-        {
-            0x0000u,
-            {
-                0u, HOST_UNUSED, 2u, HOST_UNUSED, HOST_UNUSED,
-                HOST_UNUSED, HOST_UNUSED, HOST_UNUSED, HOST_UNUSED, HOST_UNUSED
-            },
-            (sizeof(pru_intc_map) / sizeof(struct ch_map)),
-            pru_intc_map
-        }
     }
 };
