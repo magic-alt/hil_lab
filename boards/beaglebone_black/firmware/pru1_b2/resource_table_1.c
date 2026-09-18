61ee466eaa3299114b392d1020d9fc139fab95a7
kaermaxc <59996146+magic-alt@users.noreply.github.com>
2026-09-19 07:42:32 +0800

    feat: add PRU1 B2 RPMsg resource table

---
+++ boards/beaglebone_black/firmware/pru1_b2/resource_table_1.c
@@ -0,0 +1,35 @@
+#include "resource_table_1.h"
+
+#pragma DATA_SECTION(resourceTable, ".resource_table")
+#pragma RETAIN(resourceTable)
+struct hil_b2_resource_table resourceTable = {
+    { 1u, 1u, { 0u, 0u } },
+    {
+        offsetof(struct hil_b2_resource_table, rpmsg_vdev)
+    },
+    {
+        (uint32_t)TYPE_VDEV,
+        (uint32_t)VIRTIO_ID_RPMSG,
+        0u,
+        (uint32_t)RPMSG_PRU_C1_FEATURES,
+        0u,
+        0u,
+        0u,
+        2u,
+        { 0u, 0u }
+    },
+    {
+        FW_RSC_ADDR_ANY,
+        16u,
+        PRU_RPMSG_VQ0_SIZE,
+        0u,
+        0u
+    },
+    {
+        FW_RSC_ADDR_ANY,
+        16u,
+        PRU_RPMSG_VQ1_SIZE,
+        0u,
+        0u
+    }
+};
