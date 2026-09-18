d8fbdb64b442e3110a5ad0040722171a4c2d0d21
kaermaxc <59996146+magic-alt@users.noreply.github.com>
2026-09-18 19:07:10 +0800

    fix: define AM335x RPMsg resource constants

--- boards/beaglebone_black/firmware/pru0_b0/resource_table_0.h
+++ boards/beaglebone_black/firmware/pru0_b0/resource_table_0.h
@@ -6,6 +6,11 @@
 #include <rsc_types.h>
 #include <pru_rpmsg.h>
 
+#define PRU_RPMSG_VQ0_SIZE          (16u)
+#define PRU_RPMSG_VQ1_SIZE          (16u)
+#define VIRTIO_RPMSG_F_NS           (0u)
+#define RPMSG_PRU_C0_FEATURES       (1u << VIRTIO_RPMSG_F_NS)
+
 struct hil_b0_resource_table {
     struct resource_table base;
     uint32_t offset[1];
