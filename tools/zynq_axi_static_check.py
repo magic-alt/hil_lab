513b4c47ff0b2202f6f0ef24340be898bb5a82db
kaermaxc <59996146+magic-alt@users.noreply.github.com>
2026-09-19 23:17:50 +0800

    test(axi): check BRAM event FIFO source

--- tools/zynq_axi_static_check.py
+++ tools/zynq_axi_static_check.py
@@ -6,13 +6,15 @@ ROOT = Path(__file__).resolve().parents[1]
 
 FILES = {
     "rtl": ROOT / "rtl" / "control" / "hil_axi_control_plane.v",
+    "fifo": ROOT / "rtl" / "control" / "hil_axi_event_fifo.v",
     "py": ROOT / "host" / "hil" / "transports" / "register_map.py",
     "ax_dts": ROOT / "boards" / "zynq7010" / "linux" / "hil-uio.dtsi",
     "zu_dts": ROOT / "boards" / "zu2cg" / "linux" / "hil-uio.dtsi",
 }
 
 TOKENS = {
-    "rtl": ["32'h48494C32", "12'h114", "12'h118", "ram_style"],
+    "rtl": ["32'h48494C32", "12'h114", "12'h118"],
+    "fifo": ["ram_style", "DEPTH = 64"],
     "py": ["MAGIC = 0x48494C32", "REG_EVT_PUSH = 0x114", "REG_EVT_STATUS = 0x118"],
     "ax_dts": ["0x43c00000", "0x1000", "generic-uio"],
     "zu_dts": ["0xa0000000", "0x1000", "generic-uio"],
